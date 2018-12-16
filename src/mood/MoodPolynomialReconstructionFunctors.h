87c39e74608d11268afacc041e2cee41604a960d
Pierre Kestener <pierre.kestener@cea.fr>
2018-12-16 21:20:35 +0100

    minor - typo

--- src/mood/MoodPolynomialReconstructionFunctors.h
+++ src/mood/MoodPolynomialReconstructionFunctors.h
@@ -26,7 +26,7 @@ namespace mood {
  * cartesian mesh).
  *
  * Stencil has been carefuly chosen to have a number of cells sufficiently large 
- * so that the least-square system can be solved. If the stencil sisze is low, 
+ * so that the least-square system can be solved. If the stencil size is low, 
  * one can notice that the pseudo inverse can not be computer using the QR
  * decomposition.
  *
