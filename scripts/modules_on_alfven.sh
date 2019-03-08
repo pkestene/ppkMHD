661c0ab2e44fdb1bf4a1b49094929d77e1c7f62f
Pierre Kestener <pierre.kestener@cea.fr>
2019-03-08 09:09:36 +0100

    add script to load module on alfven for building ppkMHD

---
+++ scripts/modules_on_alfven.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# compiler type : GNU, INTEL, CLANG, ...
+compiler=GNU
+enable_mpi=false
+
+verbose=false
+
+# load the necessary modules
+case $compiler in
+"GNU" )
+    module load userspace
+    module load cmake
+    module load gcc/7.2.0
+    module load openmpi/gcc72
+    
+    if [ "$enable_mpi" = true ] ; then
+	export CXX=`which mpicxx`
+	export OMPI_CXX=`which g++`
+    else
+	export CXX=`which g++`
+    fi
+    ;;
+
+"INTEL" )
+    
+    # intel compiler
+    module load userspace
+    module load cmake
+    module load intel-suite
+    
+    if [ "$enable_mpi" = true ] ; then
+	# intel mpicxx is already rooted to use icpc
+	export CXX=`which mpicxx`
+    else
+	export CXX=`which icpc`
+    fi
+    
+    ;;
+
+# TODO
+# "CLANG" )
+# 	;;
+
+* )
+    printf "Wrong value for variable compiler. Available values are GNU,INTEL"
+    ;;
+esac
+
+if [ "$verbose" = true ] ; then
+    module list
+fi
+
